030e7492a9fa438da191c48e3d85e67edb209077
CVVilla <140644115+CVVilla@users.noreply.github.com>
2023-07-26 14:12:04 -0400

    Update streamlitdemo.py

--- streamlitdemo.py
+++ streamlitdemo.py
@@ -9,14 +9,24 @@ Original file is located at
 
 #download streamlit dependencies
 #pip install streamlit
-
 #IMPORT Streamlit
 import streamlit as st
+import pandas as pd
+
+
+#import our csv file
+cereals = pd.read_csv("cereal.csv")
 
 #Create a page title for the page
 #Emojis can be used as icons from https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Cereals!", page_icon=":bowl_with_spoon:", layout='wide')
-
 st.subheader("This is a Streamlit demonstration!")
 st.title("A Streamlit analysis demonstration using the Cereals Dataset!")
+
+#A selection option for choosing desired cereal brand
+cereals_name = st.selectbox("Choose a Cereal Brand", cereals['name'].unique())
+name = cereals[cereals['name'] == cereals_name]
+
+st.header("Selected: {cereals_name}")
+st.wrtie(name)
 
