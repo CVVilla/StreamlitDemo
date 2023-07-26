78b28caddc65e6105307eceaf07797634c70f2a2
CVVilla <140644115+CVVilla@users.noreply.github.com>
2023-07-26 14:32:20 -0400

    Update streamlitdemo.py

--- streamlitdemo.py
+++ streamlitdemo.py
@@ -12,10 +12,11 @@ Original file is located at
 #IMPORT Streamlit
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 #import our csv file
-cereals = pd.read_csv("cereal.csv")
+df = pd.read_csv("cereal.csv")
 
 #Create a page title for the page
 #Emojis can be used as icons from https://www.webfx.com/tools/emoji-cheat-sheet/
@@ -24,7 +25,7 @@ st.set_page_config(page_title="Cereals!", page_icon=":bowl_with_spoon:", layout=
 with st.container():
     st.title("A Streamlit analysis demonstration using the Cereals Dataset!")
     st.subheader("Below is the full dataset we will be using for our demonstration.")
-    st.write(cereals)
+    st.write(df)
 
 
 
@@ -32,9 +33,8 @@ with st.container():
 #after selecting a cereal brand, information regarding brand is displayed onto the user
 with st.container():
     st.subheader("Select a cereal brand below from the dataset")
-    cereals_name = st.selectbox("Choose a Cereal Brand", cereals['name'].unique())
-    cereal_info = cereals[cereals['name'] == cereals_name]
-
+    cereals_name = st.selectbox("Choose a Cereal Brand", df['name'].unique())
+    cereal_info = df[df['name'] == cereals_name]
     st.header(f"Selected: {cereals_name}")
     st.write(cereal_info)
 
