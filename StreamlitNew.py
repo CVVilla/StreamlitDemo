77f79f294f1fbeafb28639ebb43da469d5d4b16b
CVVilla <140644115+CVVilla@users.noreply.github.com>
2023-07-26 18:31:16 -0400

    Update StreamlitNew.py

--- StreamlitNew.py
+++ StreamlitNew.py
@@ -7,6 +7,14 @@ df = pd.read_csv("cereal.csv")
 st.set_page_config(page_title="Cereals!", page_icon=":bowl_with_spoon:", layout='wide')
 
 
+with st.container():
+    st.title("A Streamlit analysis demonstration using the Cereals Dataset!")
+    st.write("[Dataset URL](https://www.kaggle.com/datasets/crawford/80-cereals)")
+    st.write("[Streamlit Cheat Sheet](https://docs.streamlit.io/library/cheatsheet)")
+    st.subheader("Below is the full dataset we will be using for our demonstration.")
+    st.write(df)
+st.write("------------------------------------------------------------------------------")
+
 
 
 
