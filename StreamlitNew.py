5f1f27a8dca8d2a61836a3eeb538a1fa431976fe
CVVilla <140644115+CVVilla@users.noreply.github.com>
2023-07-26 18:08:40 -0400

    Update StreamlitNew.py

--- StreamlitNew.py
+++ StreamlitNew.py
@@ -3,5 +3,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
+df = pd.read_csv("cereal.csv")
 st.set_page_config(page_title="Cereals!", page_icon=":bowl_with_spoon:", layout='wide')
+
+
+
