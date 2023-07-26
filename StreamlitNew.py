f01c5d37a1273c9dad16731a824d46c2dd0f2451
CVVilla <140644115+CVVilla@users.noreply.github.com>
2023-07-26 18:57:02 -0400

    Update StreamlitNew.py

--- StreamlitNew.py
+++ StreamlitNew.py
@@ -6,7 +6,4 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("cereal.csv")
 st.set_page_config(page_title="Cereals!", page_icon=":bowl_with_spoon:", layout='wide')
 
-with st.container():
-  st.title("A Streamlit analysis demonstration using the Cereals Dataset!")
-  st.write(df)
-st.write("------------------------------------------------------------------------------")
+
