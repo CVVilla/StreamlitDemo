90292b6b0fda58d9439e3b122fd40ad3215cb5fa
CVVilla <140644115+CVVilla@users.noreply.github.com>
2023-07-26 18:16:49 -0400

    Update StreamlitNew.py

--- StreamlitNew.py
+++ StreamlitNew.py
@@ -13,3 +13,10 @@ with st.container():
    st.subheader("Below is the full dataset we will be using for our demonstration.")
    st.write(df)
 st.write("------------------------------------------------------------------------------")
+
+with st.container():
+    st.subheader("Below is a Demonstration of a Suggestive Seach bar and a drop down menu for selecting elements from the dataset")
+    st.title("Search Bar")
+    search_term = st.text_input("Search for a cereal:", "")
+    filtered_df = df[df["name"].str.contains(search_term, case=False)]
+    st.write(filtered_df)
