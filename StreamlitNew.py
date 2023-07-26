f2d0a60f8e076fd789c46fb79191e79850a93bb1
CVVilla <140644115+CVVilla@users.noreply.github.com>
2023-07-26 18:18:30 -0400

    Update StreamlitNew.py

--- StreamlitNew.py
+++ StreamlitNew.py
@@ -6,25 +6,3 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("cereal.csv")
 st.set_page_config(page_title="Cereals!", page_icon=":bowl_with_spoon:", layout='wide')
 
-
-
-with st.container():
-   st.title("A Streamlit analysis demonstration using the Cereals Dataset!")
-   st.subheader("Below is the full dataset we will be using for our demonstration.")
-   st.write(df)
-st.write("------------------------------------------------------------------------------")
-
-with st.container():
-    st.subheader("Below is a Demonstration of a Suggestive Seach bar and a drop down menu for selecting elements from the dataset")
-    st.title("Search Bar")
-    search_term = st.text_input("Search for a cereal:", "")
-    filtered_df = df[df["name"].str.contains(search_term, case=False)]
-    st.write(filtered_df)
-
-
-   st.title("Drop Down Menu")
-   cereals_name = st.selectbox("Choose a Cereal Brand", df['name'].unique()) 
-   cereal_info = df[df['name'] == cereals_name]
-   st.header(f"Selected: {cereals_name}")
-   st.write(cereal_info)
-st.write("------------------------------------------------------------------------------")
