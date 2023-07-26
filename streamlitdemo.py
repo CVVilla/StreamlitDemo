7dbb5fe7f831b54ccefafe166847b4c7bfc8d430
CVVilla <140644115+CVVilla@users.noreply.github.com>
2023-07-26 15:16:23 -0400

    Update streamlitdemo.py

--- streamlitdemo.py
+++ streamlitdemo.py
@@ -28,7 +28,7 @@ with st.container():
     st.header(f"Selected: {cereals_name}")
     st.write(cereal_info)
     
-fig, ax = plt.subplots(figsize=(10, 6))
+fig, ax = plt.subplots(figsize=(5, 3))
 ax.bar(df["name"], df["calories"])
 ax.set_xlabel("Cereal")
 ax.set_ylabel("Calories")
